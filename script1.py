83efe4e312b1ae0d3e7c672102f0b908255f629b
lucascruzestudo <lucascruzestudo@gmail.com>
2023-06-03 16:38:39 -0300

    inclusão da possibilidade de informar valores, melhorias

--- script1.py
+++ script1.py
@@ -4,12 +4,34 @@
 # Entradas: a = [1, 2, 3, ... ] e b = [4, 2, 4, ... ]
 # Saída: [2]
 
-vetor_a = [1, 2, 3, 7, 186, 9, 6, 44, 2, 1, 18, 26, 23, 73, 21, 64, 123, 83, 12, 52]
-vetor_b = [4, 2, 4, 33, 81, 123, 7, 23, 1, 79, 45, 93, 124, 93, 12, 64, 71, 23, 17, 94]
-vetor_final = []
+def set_vetor():
+    vetor = []
+    for i in range(20):
+        while True:
+            try:
+                num = int(input(f"Digite o valor {i+1}: "))
+                vetor.append(num)
+                break
+            except ValueError:
+                print("Valor inválido. Digite um número inteiro.")
+    return vetor
 
-for num in vetor_a:
-    if num in vetor_b and num not in vetor_final:
-        vetor_final.append(num)
+def separar_repetidos(vetor_a, vetor_b):
+    vetor_separado = []
+    for num in vetor_a:
+        if num in vetor_b and num not in vetor_separado:
+            vetor_separado.append(num)
+    return vetor_separado
 
-print('Valores repetidos entre os vetores: ',vetor_final)
+# Se necessário informar valores utilize a função set_vetor()
+# vetor_a = set_vetor()
+# vetor_b = set_vetor()
+
+vetor_a = [1, 2, 3, 7, 186, 9, 6, 44, 2, 1,
+           18, 26, 23, 73, 21, 64, 123, 83, 12, 52]
+vetor_b = [4, 2, 4, 33, 81, 123, 7, 23, 1, 79,
+           45, 93, 124, 93, 12, 64, 71, 23, 17, 94]
+
+vetor_separado = separar_repetidos(vetor_a, vetor_b)
+
+print('Valores repetidos entre os vetores: ', vetor_separado)
